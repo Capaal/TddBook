bc4325921ada7b6e5ef33664b4e3c4c4a38102d5
Jason Reed <jasreed.t@gmail.com>
2019-08-27 16:35:05 -0400

    Initial unit Tests.

--- function_tests.py
+++ function_tests.py
@@ -1,5 +1,42 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def testCanStartAListAndRetrieveItLater(self):
+         # User hears about our app and comes to check it out.
+        # They go to our homepage
+        self.browser.get('http://localhost:8000')
+        # They make sure this is where they want to be by checking
+        # that the title mentions to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+        # Immediately they are invited to enter a to-do item
+
+        # They enter "buy peacock feathers" into a text box (strange)
+
+        # Upon hitting enter, the page updates and lists
+        #  "1: Buy peacock feathers" as a to-do list item
+
+        # There is still a text box inviting more items
+        # They enter "Use peacock feathers to make a fly" 
+        # (Fly fishing maybe?)
+
+        # The page updates again with both items and still a text box
+
+        # They wonder where the site will remember the list.
+        # Which is when they notice the unique URL and some instructions
+
+        # They vist the URL to find their to-do list.
+
+        # Satisfied, they leave.
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
