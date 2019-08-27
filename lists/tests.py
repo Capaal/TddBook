cfbc4872d9c6763f5ed097262e98658be39f504d
Jason Reed <jasreed.t@gmail.com>
2019-08-27 17:14:13 -0400

    Sudden changes to different Django version

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def testBadMaths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    
+    def testRootUrlResolvesToHomePageView(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
