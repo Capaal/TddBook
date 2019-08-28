28ff98ff10f3c7df318edd81ad901c905ef51cee
Jason Reed <jasreed.t@gmail.com>
2019-08-28 11:03:55 -0400

    Test for working html and a super minimal basic view returning HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+    
+    def testHomePageReturnsCorrectHtml(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
     
     def testRootUrlResolvesToHomePageView(self):
         found = resolve('/')
