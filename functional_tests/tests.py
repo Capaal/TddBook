c22c0cc371cd47f0b4ffa9da45f138981b7dfc94
Jason Reed <jasreed.t@gmail.com>
2019-09-03 12:17:52 -0400

    Refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,6 +14,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def testLayoutAndStyling(self):
+        # User vists the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # And notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2, 512, delta=10)
+        
+        # Next, they start a new list, and find it centered nicely here as well
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.waitForRowInListTable('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2, 512, delta=10)
+        
     def testCanStartAListAndRetrieveItLater(self):
          # User hears about our app and comes to check it out.
         # They go to our homepage
